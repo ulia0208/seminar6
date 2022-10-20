29427ba25c7bdb996386e88823036585d142edc6
Uliana Tokareva <ulia0208@yandex.ru>
2022-10-21 01:15:05 +0300

    task 41-43

---
+++ task41/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Сколько чисел будем вводить?");
+
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа");
+int j=0;
+for (int i=0; i<n; i++)
+{
+    int m = Convert.ToInt32(Console.ReadLine());
+    
+    if (m>0)
+    {
+        j++;
+    }
+    
+}
+
+Console.WriteLine($"{j}  положительных чисел");
+
+
+
+
+
+
---
+++ task43/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.WriteLine(" Есть два уравнения"+
+"y = n1 * x + m1,"+ 
+"y = n2 * x + m2");
+
+Console.WriteLine(" Введите параметры первого уравнения");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int m1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" Введите параметры втор5 ого уравнения");
+int n2 = Convert.ToInt32(Console.ReadLine());
+int m2 = Convert.ToInt32(Console.ReadLine());
+
+ 
+
+if (n1 == n2)
+{
+    Console.WriteLine($"Прямые параллельны");
+      
+}
+    else
+{
+        
+     double x = (m2-m1)/(n1-n2);
+    double y = n2 * x + m2; 
+    Console.WriteLine($"X: {x}, Y: {y}");  
+}
+
+
